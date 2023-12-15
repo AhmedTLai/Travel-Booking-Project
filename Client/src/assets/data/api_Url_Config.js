916ceb7c08d2f3f8e0366287fe1d368626c6bf14
AhmedTLA <ahmedtlaidjia123@gmail.com>
2023-12-15 23:29:57 +0100

    port 80

--- Client/src/assets/data/api_Url_Config.js
+++ Client/src/assets/data/api_Url_Config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : 'https://bookingtravel-backend-side.onrender.com/api',
+    baseURL : 'https://booking-travel-backend-side.onrender.com/api',
     // baseURL : 'http://localhost:4000/api',
     withCredentials: true, // Include credentials in the requests
 })
